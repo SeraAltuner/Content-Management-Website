b1b74a018446db4b66610c5a0d50654d65da7932
Ece Temizkalb <ece.temizkalb@ug.bilkent.edu.tr>
2024-11-27 19:37:29 +0300

    update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = "mysql:host=localhost;port=3306;dbname=cms;charset=utf8mb4" ;
+$dsn = "mysql:host=localhost;port=3306;dbname=CMS;charset=utf8mb4" ;
 $user = "root" ;
 $pass = "root" ;
 
